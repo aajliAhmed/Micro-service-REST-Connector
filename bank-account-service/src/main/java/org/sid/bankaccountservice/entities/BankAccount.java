cbca68fb01aa4a1e47230229297bbb5d1c4286f3
AAJLI Ahmed <ahmed.conceptor@gmail.com>
2023-10-25 15:02:39 +0100

    add test

--- bank-account-service/src/main/java/org/sid/bankaccountservice/entities/BankAccount.java
+++ bank-account-service/src/main/java/org/sid/bankaccountservice/entities/BankAccount.java
@@ -1,4 +1,21 @@
 package org.sid.bankaccountservice.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.sid.bankaccountservice.enums.AccountType;
+
+import java.util.Date;
+@Entity
+@Data @NoArgsConstructor @AllArgsConstructor @Builder
 public class BankAccount {
+    @Id
+    private String id;
+    private Date createdAt;
+    private double balance;
+    private String currency;
+    private AccountType type;
 }
